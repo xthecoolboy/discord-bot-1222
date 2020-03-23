ec759917e8ac31c0b50caef9d4717bd3379fd1fa
Daniel Bulant <admin@danbulant.eu>
2020-03-23 13:59:53 +0100

    Giveaway command

---
+++ cmd/mod/giveaway.js
@@ -0,0 +1,51 @@
+const commando = require("discord.js-commando");
+const newEmbed = require("../../embed");
+
+module.exports = class Count extends commando.Command{
+    constructor(client){
+        super(client, {
+            name: "giveaway",
+            memberName: "giveaway",
+            group: "mod",
+            description: "Makes a giveaway in current channel",
+            args: [
+                {
+                    type: "channel",
+                    prompt: "Which channel to send to?",
+                    key: "channel"
+                }, {
+                    type: "integer",
+                    prompt: "How many minutes to wait?",
+                    key: "time"
+                }, {
+                    type: "string",
+                    prompt: "Which item for winning?",
+                    key: "item"
+                }
+            ]
+        })
+    }
+    async run(msg, cmd) {
+        if(!msg.member.hasPermission("ADMINISTRATOR"))
+            return msg.channel.send("You don't have permissions!");
+        const embed = newEmbed()
+        .setTitle("Giveaway! :tada:")
+        .setDescription(cmd.item)
+        .setColor("#7cfc00")
+        .setFooter(`React 🎉 to join - ${cmd.time}`)
+        .setTimestamp()
+        cmd.channel.send(embed).then(msg => {
+            msg.react("🎉")
+            setTimeout(() => {
+                const peopleReacted = msg.reactions.get("🎉").users;
+                var winner = peopleReacted.random()
+                        cmdchannel.send(`${winner} has won ${cmd.item}! :tada:`)
+                        const finalE = newEmbed()
+                        .setTitle("Giveaway Over")
+                        .setColor("#ff8c00")
+                        .setDescription(`Winner: ${winner}`)
+                        msg.edit(finalE)
+            }, cmd.time * 60000);
+        })
+    }
+}
