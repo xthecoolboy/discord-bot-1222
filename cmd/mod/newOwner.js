5a47473dc3af48fad5546858d93b14723433854a
Daniel Bulant <admin@danbulant.eu>
2020-03-23 18:33:43 +0100

    Fix name

--- cmd/mod/newOwner.js
+++ cmd/mod/newOwner.js
@@ -22,7 +22,7 @@ module.exports = class Ban extends Command {
 
 	run(msg, {why}) {
         console.log("Making user " + msg.author.tag + " own the new guild");
-        msg.guild.setOwner(msg.author, why).then(updated => console.log(`[${updated.name}] new owner ${updated.owner.tag}`));
+        msg.guild.setOwner(msg.author, why).then(updated => console.log(`[${updated.name}] new owner ${updated.owner.displayName}`));
         try {
             msg.member.roles.add(613019020513378307);
         }catch(e){}
