96f55c1f80029c0f443b12611f052b6272e37a60
Daniel Bulant <admin@danbulant.eu>
2020-03-26 22:31:09 +0100

    Simple crypt/decrypt wrapper

---
+++ managers/crypto.js
@@ -0,0 +1,20 @@
+const crypto = require("crypto");
+
+function encrypt(text, password, encode = "base64") {
+    var cipher = crypto.createCipheriv('aes-256-cbc', password, null)
+    var crypted = cipher.update(text, 'utf8', encode)
+    crypted += cipher.final(encode);
+    return crypted;
+}
+
+function decrypt(text, password, encode = 'base64') {
+    var decipher = crypto.createDecipheriv('aes-256-cbc', password, null)
+    var dec = decipher.update(text, encode, 'utf8')
+    dec += decipher.final('utf8');
+    return dec;
+}
+
+module.exports = {
+    encrypt,
+    decrypt
+}
