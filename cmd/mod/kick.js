f5687d558f4efbcab41649170cb10a703573aa9b
Daniel Bulant <admin@danbulant.eu>
2020-03-27 10:55:15 +0100

    Kick command, copying ban

---
+++ cmd/mod/kick.js
@@ -0,0 +1,42 @@
+const { Command } = require('discord.js-commando');
+const newEmbed = require("../../embed");
+
+module.exports = class Ban extends Command {
+	constructor(client) {
+		super(client, {
+			name: 'kick',
+			group: 'mod',
+			memberName: 'kick',
+			description: 'Kicks user from server',
+			args: [
+				{
+					key: 'user',
+					prompt: 'Who should be kicked?',
+					type: 'user'
+				},{
+                    key: "reason",
+                    prompt: "Why do you want to kick the user?",
+                    type: "string",
+                    default: "No reason provided"
+                }
+			]
+		});
+	}
+
+	run(msg, { user, reason }) {
+		if (this.client.isOwner(user.id)) return msg.channel.send('the bot owner can not be kicked.');
+
+        if(this.client.user == user)return msg.channel.send("You can't kick me!");
+
+        if(!msg.guild.member(user).kickable)return msg.channel.send("You can't ban this user");
+
+        msg.guild.member(user).kick(reason);
+
+		var embed = newEmbed();
+
+		embed.setTitle(user.tag + " kicked");
+		embed.setAuthor(msg.author.tag, msg.author.avatarURL);
+
+        msg.channel.send(embed);
+    }
+}
