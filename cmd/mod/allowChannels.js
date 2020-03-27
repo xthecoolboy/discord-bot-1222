14425377c32fb024551dfb7f9ec7b0c6b4123c64
Daniel Bulant <admin@danbulant.eu>
2020-03-27 11:34:07 +0100

    Fix the command so it works

---
+++ cmd/mod/allowChannels.js
@@ -0,0 +1,35 @@
+const commando = require("discord.js-commando");
+
+module.exports = class AllowChannels extends commando.Command {
+    constructor(client){
+        super(client, {
+            name: "allowchannels",
+            aliases: ["allow-channels"],
+            memberName: "allowchannels",
+            group: "mod",
+            description: "Lists allowed channels or sets them",
+            userPermissions: ["MANAGE_CHANNELS"],
+            args: [
+                {
+                    prompt: "Which channels to allow?",
+                    default: "",
+                    key: "channels",
+                    infinite: true,
+                    type: "channel"
+                }
+            ]
+        })
+    }
+
+    run(msg, {channels}){
+        var allowedChannels = [];
+
+        for(var channel of channels){
+            allowedChannels.push(channel.id)
+        }
+
+        msg.guild.settings.set("allowedChannels", allowedChannels);
+
+        msg.channel.send("Done!");
+    }
+}
