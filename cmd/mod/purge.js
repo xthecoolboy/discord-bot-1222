b64aab17b46b53880ca4fc5415b202eb80201731
noahmanscher <61208797+noahmanscher@users.noreply.github.com>
2020-03-28 14:24:04 +0100

    Create purge.js

---
+++ cmd/mod/purge.js
@@ -0,0 +1,51 @@
+const { Command } = require('discord.js-commando');
+const newEmbed = require("../../embed");
+
+module.exports = class purgeCommand extends Command {
+    constructor(client) {
+        super(client, {
+            name: 'purge',
+            group: 'mod',
+            memberName: 'purge',
+            description: 'Deletes a certain amount of message in channel',
+			clientPermissions: ["MANAGE_MESSAGES"],
+			userPermissions: ["MANAGE_MESSAGES"],
+            args: [
+                {
+                type: 'integer',
+                key: 'amount',
+                prompt: 'how many messages do you want to delete?',
+                default: '',
+                }
+            ]
+        })
+    }
+
+    run(msg, cmd) {
+
+        if (cmd.amount > 0 & cmd.amount < 100) {
+
+            try {
+                msg.channel.bulkDelete(cmd.amount + 1);
+                    
+                var embed = newEmbed();
+                embed.setDescription(`✅ Successfully purged ${cmd.amount} messages!`);
+                embed.setAuthor(msg.author.username, msg.author.displayAvatarURL);
+                
+                return msg.channel.send(embed)
+                .then(msg => {
+                    msg.delete(3000);
+                });
+            }
+
+            catch(e) {
+                msg.channel.send(`Something went wrong...`)
+                console.error(e);
+            }
+
+        } else {
+            return msg.channel.send('Please select a valid amount between 1 and 99!')
+        }
+    }
+
+};
