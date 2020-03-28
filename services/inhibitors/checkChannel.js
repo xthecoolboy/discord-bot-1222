0ae6f71a45e198f4749c9e6500b69b66806c8f05
Daniel Bulant <admin@danbulant.eu>
2020-03-28 15:32:18 +0100

    Fix DMs

--- services/inhibitors/checkChannel.js
+++ services/inhibitors/checkChannel.js
@@ -1,7 +1,8 @@
 module.exports = msg => {
-    if(msg.guild.settings.get("allowedChannels", false)){
-        if(!msg.guild.settings.get("allowedChannels").includes(msg.channel.id)){
-            return "You can't use Ice in this channel!";
+    if(msg.guild)
+        if(msg.guild.settings.get("allowedChannels", false)){
+            if(!msg.guild.settings.get("allowedChannels").includes(msg.channel.id)){
+                return "You can't use Ice in this channel!";
+            }
         }
-    }
 }
