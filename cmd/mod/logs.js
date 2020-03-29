60ac3a1a13d78f5b70b9d1ffe0da2b3190c179ef
Daniel Bulant <admin@danbulant.eu>
2020-03-29 18:04:32 +0200

    Try saving as json string

--- cmd/mod/logs.js
+++ cmd/mod/logs.js
@@ -74,24 +74,24 @@ module.exports = class Logs extends commando.Command {
         /* eslint-disable no-redeclare */
         switch (command) {
             case "set":
-                var channels = msg.guild.settings.get("logs-channels", []);
+                var channels = JSON.parse(msg.guild.settings.get("logs-channels", "[]"));
                 channels.push({
                     options: ["*"],
                     channel: channel.id
                 });
                 console.log(channels);
-                msg.guild.settings.set("logs-channel", channels);
-                console.log(msg.guild.settings.get("logs-channels", null));
+                msg.guild.settings.set("logs-channel", JSON.stringify(channels));
+                console.log(JSON.parse(msg.guild.settings.get("logs-channels", null)));
                 msg.channel.send("New channel added with default settings");
                 break;
             case "remove":
-                var channels = msg.guild.settings.get("logs-channels", []);
+                var channels = JSON.parse(msg.guild.settings.get("logs-channels", "[]"));
                 channels = channels.filter(ch => ch.channel !== channel.id);
                 msg.guild.settings.set("logs-channel", channels);
                 msg.channel.send("Removed the channel");
                 break;
             case "alter":
-                var channels = msg.guild.settings.get("logs-channels", []);
+                var channels = JSON.parse(msg.guild.settings.get("logs-channels", "[]"));
                 var ch = channels.filter(ch => ch.channel === channel.id)[0];
                 if (!ch) return msg.channel.send("This channel is not set as logging channel!");
 
@@ -132,7 +132,7 @@ module.exports = class Logs extends commando.Command {
             case "view":
                 var embed = newEmbed();
                 embed.setTitle("Logging channel `#" + channel.name + "`");
-                var ch = msg.guild.settings.get("logs-channels", []);
+                var ch = JSON.parse(msg.guild.settings.get("logs-channels", "[]"));
                 ch = ch.filter(c => c.channel === channel.id)[0];
                 if (!ch) {
                     embed.setDescription("The channel <#" + channel.id + "> is not setup as channel for logs!");
