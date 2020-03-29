315a315e10d450bea60721ed99ff7608e8a11c7b
noahmanscher <61208797+noahmanscher@users.noreply.github.com>
2020-03-29 15:34:47 +0200

    Create clearsettings.js

---
+++ cmd/mod/clearsettings.js
@@ -0,0 +1,33 @@
+const { Command } = require('discord.js-commando');
+
+module.exports = class clearsettingsCommand extends Command {
+    constructor(client) {
+        super(client, {
+            name: 'clearsettings',
+            group: 'mod',
+            memberName: 'clearsettings',
+            description: 'Clears all the guilds settings',
+            userPermissions: ["ADMINISTRATOR"],
+            args: [
+                {
+                    key: 'confirmation',
+                    type: 'string',
+                    oneOf: ['confirm'],
+                    prompt: 'WARNING - this will delete all the guilds cases and other settings! Are you sure? Type `confirm` to confirm.'
+                }
+            ]
+        })
+    }
+
+    run(msg) {
+        try {
+            msg.guild.settings.clear();
+            msg.say('Success!');
+        }
+
+        catch(e) {
+            console.error(e);
+            msg.say('Something went wrong...');
+        }
+    }
+};
