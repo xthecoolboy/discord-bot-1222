2d3c76abd03f7dfb8d318b1eefcbe4d0e6537905
noahmanscher <61208797+noahmanscher@users.noreply.github.com>
2020-03-29 15:27:22 +0200

    Add suffix function (st, nd, rd, th)

--- utils.js
+++ utils.js
@@ -27,9 +27,25 @@ function insertAt(a, b, position) {
 
 const assistant_icon = "Please wait";
 
+function suffix(i) {
+    var j = i % 10,
+        k = i % 100;
+    if (j == 1 && k != 11) {
+        return i + "st";
+    }
+    if (j == 2 && k != 12) {
+        return i + "nd";
+    }
+    if (j == 3 && k != 13) {
+        return i + "rd";
+    }
+    return i + "th";
+}
+
 module.exports = {
     shortNumber,
     assistant_icon,
     padWithZeroes,
-    insertAt
+    insertAt,
+    suffix
 }
