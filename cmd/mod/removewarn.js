5241ca833292be83eb4cff990e2e42d2c9d74fe1
noahmanscher <61208797+noahmanscher@users.noreply.github.com>
2020-03-29 17:20:59 +0200

    Update removewarn.js

--- cmd/mod/removewarn.js
+++ cmd/mod/removewarn.js
@@ -22,15 +22,16 @@ module.exports = class removewarnCommand extends Command {
     run (msg, cmd) {
         const Case = msg.guild.settings.get(`case.${cmd.case}`);
         if (!Case) return msg.say(`Case '${cmd.case}' not found`);
+        if (Case.removed) return msg.say("This warn has already been removed!");
+
+        Case.removed = true;
 
         const warnCount = msg.guild.settings.get(`warns.${Case.offenderID}`, 1);
         msg.guild.settings.set(`warns.${Case.offenderID}`, warnCount - 1);
 
-        msg.guild.settings.set(`case.${cmd.case}`, "");
-
         const embed = newEmbed();
         embed.setAuthor(`${msg.author.username} | Case ${Case.id}`, msg.author.displayAvatarURL);
-        embed.setDescription(`✅ Successfully removed warning from user ${Case.offender}`);
+        embed.setDescription(`✅ Successfully removed warning from user <@${Case.offenderID}>`);
         return msg.embed(embed);
     }
 };
