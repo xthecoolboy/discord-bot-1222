4d4b441ad2d7eb9328446de66223cbb614056d22
noahmanscher <noahmanscher@gmail.com>
2020-03-30 11:16:58 +0200

    add option to delete cases

--- cmd/mod/case.js
+++ cmd/mod/case.js
@@ -22,14 +22,22 @@ module.exports = class caseCommand extends Command {
         const Case = msg.guild.settings.get(`case.${cmd.case}`);
         if (!Case) return msg.say(`Case '${cmd.case}' not found`);
 
-        let removedText = "";
-        if (Case.removed) removedText = "**[REMOVED]**";
+        if (!cmd.delete) {
+            let removedText = "";
+            if (Case.removed) removedText = "**[REMOVED]**";
 
-        const embed = newEmbed();
-        embed.setTitle(`${Case.type} | case ${Case.id} ${removedText}`);
-        embed.addField("Offender", `${Case.offender.tag} <@${Case.offender.id}>`);
-        embed.addField("Reason", Case.reason);
-        embed.addField("Responsible moderator:", `${Case.moderator.tag} <@${Case.moderator.id}>`);
-        return msg.embed(embed);
+            const embed = newEmbed();
+            embed.setTitle(`${Case.type} | case ${Case.id} ${removedText}`);
+            embed.addField("Offender", `${Case.offender} <@${Case.offenderID}>`);
+            embed.addField("Reason", Case.reason);
+            embed.addField("Responsible moderator:", `${Case.moderator} <@${Case.moderatorID}>`);
+            return msg.embed(embed);
+        } else {
+            msg.guild.settings.remove(`case.${cmd.case}`);
+            const embed = newEmbed();
+            embed.setColor("RED");
+            embed.setTitle(`✅ Successfully deleted case **${cmd.case}**`);
+            return msg.embed(embed);
+        }
     }
 };
