02a426eacbe16912e99c26968089657e8f940413
Daniel Bulant <admin@danbulant.eu>
2020-04-03 12:58:31 +0200

    Better loading

--- services/logging/registerEvents.js
+++ services/logging/registerEvents.js
@@ -1,13 +1,20 @@
 const log = require("./logger");
 
-module.exports = client => {
-    client.on("messageUpdate", (old, msg) => {
-        if(msg.author.bot) return;
-        if(msg.channel.id === "692839951611723877" && client.user.id !== "527453262639792138") return;
+const mappings = [
 
-        log(msg, "message.edit", {
-            old,
-            msg
+];
+
+const map = new Map(mappings);
+
+module.exports = client => {
+    for(var event in map) {
+        var eventName = map[event];
+
+        client.on(event, (msg, ...args) => {
+            if(msg.author.bot) return;
+            if(msg.channel.id === "692839951611723877" && client.user.id !== "527453262639792138") return;
+
+            log(msg, eventName, [msg, ...args]);
         });
-    });
+    }
 };
