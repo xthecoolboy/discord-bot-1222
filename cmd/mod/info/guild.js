9c84c4aa92d51034192c190f8d7fa4e19ee501bb
noahmanscher <noahmanscher@gmail.com>
2020-04-06 14:07:23 +0200

    Server Info Command

--- cmd/mod/info/guild.js
+++ cmd/mod/info/guild.js
@@ -1,5 +1,20 @@
 const newEmbed = require("../../../embed");
 
 module.exports = (msg, cmd) => {
-    msg.channel.send(newEmbed().setTitle("To be done"));
+    const embed = newEmbed();
+    embed.setAuthor(msg.author.tag, msg.author.avatarURL);
+    embed.setTitle(msg.guild.name);
+    embed.setThumbnail(msg.guild.iconURL);
+    embed.addField("Owner", msg.guild.owner, true)
+        .addField("Region", msg.guild.region, true)
+        .addField("Categories", msg.guild.channels.filter(c => c.type === "category").size, true)
+        .addField("Text Channels", msg.guild.channels.filter(c => c.type === "text").size, true)
+        .addField("Voice Channels", msg.guild.channels.filter(c => c.type === "voice").size, true)
+        .addField("Roles", msg.guild.roles.size, true)
+        .addField("Members", msg.guild.members.size, true)
+        .addField("Humans", msg.guild.members.filter(m => !m.user.bot).size, true)
+        .addField("Bots", msg.guild.members.filter(m => m.user.bot).size, true);
+    if(msg.guild.bannerURL) embed.setImage(msg.guild.bannerURL);
+    embed.setFooter(`${embed.footer.text} | Server Created: ${msg.guild.createdAt.getDate()}/${msg.guild.createdAt.getMonth()}/${msg.guild.createdAt.getFullYear()}`);
+    return msg.embed(embed);
 };
