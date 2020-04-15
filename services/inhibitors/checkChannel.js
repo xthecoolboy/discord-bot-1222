11ac9d7add78eef24bcb12aa38e9457dc14e186f
Daniel Bulant <admin@danbulant.eu>
2020-04-15 19:00:33 +0200

    Allow lonely dev

--- services/inhibitors/checkChannel.js
+++ services/inhibitors/checkChannel.js
@@ -1,5 +1,4 @@
 module.exports = msg => {
-    if(msg.author.id === "609343924468842517") return "No Lonely devs allowed";
     if(msg.guild) {
         if(msg.guild.settings.get("allowedChannels", false)) {
             if(!msg.guild.settings.get("allowedChannels").includes(msg.channel.id)) {
