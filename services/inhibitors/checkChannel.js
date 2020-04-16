5811fb190c32101eb61d971b849e62e7e22db4c3
Daniel Bulant <admin@danbulant.eu>
2020-04-16 10:02:48 +0200

    Fix blacklisting users

--- services/inhibitors/checkChannel.js
+++ services/inhibitors/checkChannel.js
@@ -1,4 +1,6 @@
-module.exports = msg => {
+module.exports = async msg => {
+    var blacklist = await msg.client.provider.get("global", "userBlacklist", []);
+    if(blacklist.includes(msg.author.id)) return "blacklisted";
     if(msg.guild) {
         if(msg.guild.settings.get("allowedChannels", false)) {
             if(!msg.guild.settings.get("allowedChannels").includes(msg.channel.id)) {
