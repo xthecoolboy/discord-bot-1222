5c150ec88d20827974842dd504341cb2cd1e1a6f
Daniel Bulant <admin@danbulant.eu>
2020-04-21 12:14:41 +0200

    Add more server endpoints

--- services/server.js
+++ services/server.js
@@ -14,6 +14,81 @@ app.get("/stats", (req, res) => {
     });
 });
 
+app.get("/guild/:id", (req, res) => {
+    var guild = client.guilds.resolve(req.params.id);
+    if(guild) {
+        res.json(guild);
+    } else {
+        res.status(404).json({
+            error: "not_found"
+        });
+    }
+});
+
+app.get("/member/:guild/:id", (req, res) => {
+    var guild = client.guilds.resolve(req.params.guild);
+    if(guild) {
+        var member = guild.member(req.params.id);
+        var user = client.users.resolve(req.params.id);
+        if(member) {
+            res.json({
+                member,
+                user
+            });
+        } else {
+            res.status(404).json({
+                error: "not_found"
+            });
+        }
+    } else {
+        res.status(404).json({
+            error: "guild_not_found"
+        });
+    }
+});
+
+app.get("/channel/:guild/:id", (req, res) => {
+    var guild = client.guilds.resolve(req.params.guild);
+    if(guild) {
+        var channel = guild.channels.resolve(req.params.id);
+        if(channel) {
+            res.json(channel);
+        } else {
+            res.status(404).json({
+                error: "not_found"
+            });
+        }
+    } else {
+        res.status(404).json({
+            error: "guild_not_found"
+        });
+    }
+});
+
+app.get("/message/:guild/:channel/:id", async (req, res) => {
+    try {
+        var guild = client.guilds.resolve(req.params.guild);
+        var channel = guild.channels.resolve(req.params.channel);
+        var message = await channel.messages.fetch(req.params.id);
+        res.json(message);
+    } catch(e) {
+        res.status(404).json({
+            error: "not_found"
+        });
+    }
+});
+
+app.get("/user/:id", (req, res) => {
+    var user = client.users.resolve(req.params.id);
+    if(user) {
+        res.json(user);
+    } else {
+        res.status(404).json({
+            error: "not_found"
+        });
+    }
+});
+
 app.get("/", (req, res) => {
     res.write(new Date().toJSON());
     res.end();
