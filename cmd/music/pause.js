642e72ca7ea1a132297aea94d08893a1421e3796
Daniel Bulant <admin@danbulant.eu>
2020-04-28 15:23:33 +0200

    Implement pause command

---
+++ cmd/music/pause.js
@@ -0,0 +1,27 @@
+const commando = require("@iceprod/discord.js-commando");
+const player = require("../../services/player/player");
+
+module.exports = class Pause extends commando.Command {
+    constructor(client) {
+        super(client, {
+            name: "pause",
+            memberName: "pause",
+            group: "music",
+            description: "Pauses playback"
+        });
+    }
+
+    async run(msg) {
+        if(player.isPaused(msg.guild)) {
+            return msg.channel.send("Playback is already paused. Use resume to continue playing.");
+        }
+        try {
+            await player.pause(msg.guild);
+            msg.channel.send("Paused");
+        } catch(e) {
+            console.log(e);
+
+            msg.channel.send("Couldn't pause playback as nothing is playing");
+        }
+    }
+};
