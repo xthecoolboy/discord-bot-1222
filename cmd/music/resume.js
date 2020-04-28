b013d11fc1397832882c300885d1ada572b8ff5b
Daniel Bulant <admin@danbulant.eu>
2020-04-28 15:23:39 +0200

    Implement resume command

---
+++ cmd/music/resume.js
@@ -0,0 +1,27 @@
+const commando = require("@iceprod/discord.js-commando");
+const player = require("../../services/player/player");
+
+module.exports = class Resume extends commando.Command {
+    constructor(client) {
+        super(client, {
+            name: "resume",
+            memberName: "resume",
+            group: "music",
+            description: "Resumes playback"
+        });
+    }
+
+    async run(msg) {
+        if(!player.isPaused(msg.guild)) {
+            return msg.channel.send("Playback is already playing. Use `pause` to pause playback.");
+        }
+        try {
+            await player.pause(msg.guild);
+            msg.channel.send("Resumed");
+        } catch(e) {
+            console.log(e);
+
+            msg.channel.send("Couldn't resume playback as nothing is playing");
+        }
+    }
+};
