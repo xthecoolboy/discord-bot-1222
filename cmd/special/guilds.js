0d3b4d3db25867a9542c5f9404f6a81d0ddc58bb
Daniel Bulant <admin@danbulant.eu>
2020-04-28 10:46:07 +0200

    Add guilds listing

---
+++ cmd/special/guilds.js
@@ -0,0 +1,38 @@
+const { Command } = require("@iceprod/discord.js-commando");
+const pages = require("../../managers/pages");
+const newEmbed = require("../../embed");
+
+module.exports = class Guilds extends Command {
+    constructor(client) {
+        super(client, {
+            name: "guilds",
+            aliases: ["guilds"],
+            group: "special",
+            memberName: "guilds",
+            ownerOnly: true,
+            description: "Lists guilds ice is in",
+            throttling: {
+                usages: 2,
+                duration: 3
+            }
+        });
+    }
+
+    async run(msg) {
+        var guilds = msg.client.guilds.cache;
+
+        guilds = guilds.sort((a, b) => a.memberCount - b.memberCount);
+
+        guilds = guilds.array();
+
+        var embed = newEmbed();
+        embed.setTitle("Top guilds");
+
+        embed.addField("Guilds", "loading...");
+
+        await pages(msg, embed, (await msg.channel.send(embed)), guilds, 1, "guilds",
+            guild => {
+                return `${guild.name} (${guild.memberCount})`;
+            });
+    }
+};
