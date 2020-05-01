6864deb7fec3973a845e0d1fe77f664bcd85e283
Daniel Bulant <admin@danbulant.eu>
2020-05-01 14:43:21 +0200

    Vote check command

---
+++ cmd/essentials/voted.js
@@ -0,0 +1,21 @@
+const commando = require("@iceprod/discord.js-commando");
+
+module.exports = class Voted extends commando.Command {
+    constructor(client) {
+        super(client, {
+            name: "voted",
+            aliases: ["vote"],
+            memberName: "voted",
+            group: "essentials",
+            description: "Checks your vote status"
+        });
+    }
+
+    async run(msg) {
+        if(await msg.author.hasVoted()) {
+            msg.channel.send("You already voted, all vote-locked commands are available!");
+        } else {
+            msg.channel.send("You haven't voted yet. You can vote at https://top.gg/bot/" + this.client.id);
+        }
+    }
+};
