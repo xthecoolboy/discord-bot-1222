4a24c1b3c233916c63b85bc8ed6a3bf35c9ca56d
Daniel Bulant <admin@danbulant.eu>
2020-05-01 15:53:58 +0200

    fix invite link

--- cmd/essentials/voted.js
+++ cmd/essentials/voted.js
@@ -15,7 +15,7 @@ module.exports = class Voted extends commando.Command {
         if(await msg.author.hasVoted()) {
             msg.channel.send("You already voted, all vote-locked commands are available!");
         } else {
-            msg.channel.send("You haven't voted yet. You can vote at https://top.gg/bot/" + this.client.id);
+            msg.channel.send("You haven't voted yet. You can vote at https://top.gg/bot/" + this.client.user.id);
         }
     }
 };
