1d68937c0519381ac0602ee3b98ab6fc8adc7819
Daniel Bulant <admin@danbulant.eu>
2020-05-02 12:19:48 +0200

    Better invite URL

--- cmd/essentials/invite.js
+++ cmd/essentials/invite.js
@@ -11,7 +11,8 @@ module.exports = class Invite extends commando.Command {
     }
 
     run(msg) {
-        const inviteURL = "https://discordapp.com/api/oauth2/authorize?client_id=654725534365909043&permissions=8&scope=bot";
+        // const inviteURL = "https://discordapp.com/api/oauth2/authorize?client_id=654725534365909043&permissions=8&scope=bot";
+        const inviteURL = "https://top.gg/" + this.client.user.id;
         msg.channel.send(inviteURL);
     }
 };
