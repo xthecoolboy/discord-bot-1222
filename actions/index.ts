5058bcf01852fe7e50b6c96caf0ea4bc80f5d2f9
Daniel Bulant <admin@danbulant.eu>
2020-05-08 17:05:36 +0200

    Try making test for the lib

--- actions/index.ts
+++ actions/index.ts
@@ -1,1 +1,16 @@
 #!/usr/bin/env deno
+
+import { Channel, Client } from "./lib/main.ts";
+
+
+var client = await Client.newClient("692837502117216307", "654725534365909043");
+
+var channel = new Channel({
+    client,
+    id: "692839951611723877"
+});
+
+console.log("Sending test message");
+var msg = await channel.send("Hello world from actions!");
+console.log(msg);
+console.log("Done!");
