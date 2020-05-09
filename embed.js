1b837f10c0a7c84b12b98630527c1fb5fe9e423c
Daniel Bulant <admin@danbulant.eu>
2020-05-09 11:29:19 +0200

    Fix authors

--- embed.js
+++ embed.js
@@ -5,6 +5,6 @@ module.exports = () => {
         // .attachFiles(['./stefanik_vlajka.jpg'])
         // .setAuthor('ČSK Bot', 'attachment://stefanik_vlajka.jpg', 'https://danbulant.eu')
         // .setTimestamp()
-        .setFooter("© Aztec, TechmandanCZ#0135 and Noah#6809", "https://cdn.discordapp.com/attachments/701351893661777930/707851114543972432/aztec.jpg");
+        .setFooter("© Aztec, made by Ice Productions", "https://cdn.discordapp.com/attachments/701351893661777930/707851114543972432/aztec.jpg");
     return embed;
 };
