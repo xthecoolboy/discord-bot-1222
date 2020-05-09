0a717177c82a7ff4c78fa9182fe73e3cbaf73784
noahmanscher <noahmanscher@gmail.com>
2020-05-09 14:10:25 +0200

    temp fix for channel checking

--- services/inhibitors/checkChannel.js
+++ services/inhibitors/checkChannel.js
@@ -4,6 +4,16 @@ module.exports = async msg => {
         return "blacklisted";
     }
     if(msg.guild) {
+        if(msg.content === await msg.guild.settings.get("prefix", msg.client.commandPrefix) + "channels clear" && msg.member.permissions.has("MANAGE_CHANNELS")) {
+            msg.guild.settings.set("allowedChannels", [])
+                .then(msg.channel.send("All channels are now allowed :smile:"))
+                .catch(e => {
+                    console.log(e);
+                    msg.channel.send("Something went wrong...");
+                });
+            return "Channels cleared";
+        }
+
         if(msg.guild.settings.get("allowedChannels", []).length) {
             if(!msg.guild.settings.get("allowedChannels").includes(msg.channel.id)) {
                 return "You can't use Ice in this channel!";
