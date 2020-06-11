fcafad460deedcd99dadca070c322295f8694849
Daniel Bulant <admin@danbulant.eu>
2020-06-11 14:35:46 +0200

    Fix formatting

--- .eslintrc.js
+++ .eslintrc.js
@@ -31,6 +31,9 @@ module.exports = {
       before: false,
       after: false,
       overrides: {
+        of: {
+          after: true
+        },
         var: {
           after: true
         },
