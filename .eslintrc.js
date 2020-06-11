0432724c571f0527befdfe0c2b04773abd98e2ca
Daniel Bulant <admin@danbulant.eu>
2020-06-11 15:40:59 +0200

    Break formatting

--- .eslintrc.js
+++ .eslintrc.js
@@ -31,6 +31,9 @@ module.exports = {
       before: false,
       after: false,
       overrides: {
+        break: {
+          before: true
+        },
         of: {
           after: true
         },
