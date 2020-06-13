d70858e0a9eb81b2bb9afdc7d08121dbf2a27365
Daniel Bulant <admin@danbulant.eu>
2020-06-13 11:38:12 +0200

    Fix syntax

--- actions/eventManagerFunc.sql
+++ actions/eventManagerFunc.sql
@@ -1,5 +1,7 @@
 DELIMITER $$
--- Done!
+
+DROP FUNCTION IF EXISTS SPLIT$$
+
 CREATE FUNCTION SPLIT(
     string VARCHAR(2048),
     delimeter VARCHAR(32),
@@ -12,6 +14,8 @@ END;$$
 
 DELIMETER ;
 DELIMITER $$
+
+DROP FUNCTION IF EXISTS MatchRule$$
 
 CREATE FUNCTION MatchRule(
     string VARCHAR(256),
@@ -28,18 +32,19 @@ BEGIN
 
     SET currentPointer = 1;
 
-    SET string_length = 1 + LENGTH(string) - LENGTH(REPLACE(string, '.', ''));
-    SET rule_length = 1 + LENGTH(rule) - LENGTH(REPLACE(rule, '.', ''));
+    SET string_length = LENGTH(string) - LENGTH(REPLACE(string, '.', ''));
+    SET rule_length = LENGTH(rule) - LENGTH(REPLACE(rule, '.', ''));
 
     IF string = rule THEN return (true); END IF;
-    match_loop: WHILE currentPointer < MIN(string_length, rule_length) DO
+
+    match_loop: WHILE currentPointer <= LEAST(string_length, rule_length) DO
         SET stringChunk = TRIM(SPLIT(string, '.', currentPointer));
         SET ruleChunk = TRIM(SPLIT(rule, '.', currentPointer));
         SET currentPointer = currentPointer + 1;
       
-        if(ruleChunk = stringChunk) ITERATE match_loop;
-        if(ruleChunk = '*') ITERATE match_loop;
-        if(ruleChunk = '**') LEAVE match_loop;
+        if(ruleChunk = stringChunk) THEN ITERATE match_loop; END IF;
+        if(ruleChunk = '*') THEN ITERATE match_loop; END IF;
+        if(ruleChunk = '**') THEN LEAVE match_loop; END IF;
         return (false);
     END WHILE;
 
@@ -48,6 +53,8 @@ END$$
 
 DELIMETER ;
 DELIMITER $$
+
+DROP FUNCTION IF EXISTS CheckRuleset$$
 
 CREATE FUNCTION CheckRuleset(
     string VARCHAR(256),
@@ -58,37 +65,37 @@ DETERMINISTIC
 BEGIN
     DECLARE matches boolean;
     DECLARE rulesChunk VARCHAR(64);
-    DECLARE enabled VARCHAR(1024);
-    DECLARE disabled VARCHAR(1024);
+    DECLARE r_enabled VARCHAR(1024);
+    DECLARE r_disabled VARCHAR(1024);
     DECLARE rules_length INTEGER(11);
     DECLARE currentPointer INTEGER(11);
 
+    SET matches = false;
+
     SET currentPointer = 1;
-    SET enabled = SPLIT(rules, '!', 1);
-    IF 1 + LENGTH(rules) - LENGTH(REPLACE(rules, '!', '') > 1 THEN
-        SET disabled = SPLIT(rules, '!', 2);
+    SET r_enabled = SPLIT(rules, '!', 1);
+    IF (1 + LENGTH(rules) - LENGTH(REPLACE(rules, '!', '')) > 1) THEN
+        SET r_disabled = SPLIT(rules, '!', 2);
     END IF;
 
-    SET rules_length = 1 + LENGTH(enabled) - LENGTH(REPLACE(enabled, '|', ''));
+    SET rules_length = 1 + LENGTH(r_enabled) - LENGTH(REPLACE(r_enabled, '|', ''));
 
-    IF string = rule THEN return (true); END IF
     match_loop: WHILE currentPointer < rules_length DO
         SET currentPointer = currentPointer + 1;
-        IF(MatchRule(string, SPLIT(enabled, '|', currentPointer - 1))) THEN
+        IF(MatchRule(string, SPLIT(r_enabled, '|', currentPointer - 1))) THEN
             SET matches = true;
             LEAVE match_loop;
         END IF;
     END WHILE;
 
-    if(disabled IS NULL) THEN RETURN (matches); END IF;
+    if(r_disabled IS NULL) THEN RETURN (matches); END IF;
 
-    rules_length = 1 + LENGTH(disabled) - LENGTH(REPLACE(disabled, '|', ''));
+    SET rules_length = 1 + LENGTH(r_disabled) - LENGTH(REPLACE(r_disabled, '|', ''));
     SET currentPointer = 1;
 
-    IF string = rule THEN return (true); END IF
     dematch_loop: WHILE currentPointer < rules_length DO
         SET currentPointer = currentPointer + 1;
-        IF(MatchRule(string, SPLIT(disabled, '|', currentPointer - 1))) THEN
+        IF(MatchRule(string, SPLIT(r_disabled, '|', currentPointer - 1))) THEN
             SET matches = false;
             LEAVE dematch_loop;
         END IF;
