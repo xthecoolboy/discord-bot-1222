7fb260aeeb0db7d1b92b2c8a47a95815b5369515
Daniel Bulant <admin@danbulant.eu>
2020-06-13 13:13:35 +0200

    Add action handler

--- services/logging/registerEvents.js
+++ services/logging/registerEvents.js
@@ -1,4 +1,10 @@
 const log = require("./logger");
+var actionHandler;
+try {
+    actionHandler = require("../actions/handle");
+} catch(e) {
+    console.log("Actions couldn't be laoded, skipping integration");
+}
 const fs = require("fs");
 const path = require("path");
 
@@ -10,7 +16,7 @@ const map = new Map();
     for(var line of mapping.split("\n")) {
         line = line.split(":");
         if(!line[1] || line[1] === "null") continue;
-        map.set(line[0], line[1]);
+        map.set(line[0].trim(), line[1].trim());
     }
 })();
 
@@ -18,6 +24,7 @@ module.exports = client => {
     for(const [event, eventName] of map) {
         client.on(event, (...args) => {
             log(event, eventName, [...args]);
+            if(actionHandler) actionHandler(event, eventName, [...args]);
         });
     }
 };
