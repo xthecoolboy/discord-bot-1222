358b99b495d175bb8f93130bd90b32dbf097802c
Daniel Bulant <admin@danbulant.eu>
2020-06-13 13:39:34 +0200

    Run the actual code

--- services/actions/handle.js
+++ services/actions/handle.js
@@ -7,6 +7,9 @@ form.
 
 const { getGuild } = require("../../utils");
 const client = require("../../managers/pool_mysql");
+const { exec } = require("child_process");
+const { join } = require("path");
+const sl = require("singleline");
 
 module.exports = async (realEvent, event, data) => {
     const guild = getGuild(realEvent, data);
@@ -17,7 +20,57 @@ module.exports = async (realEvent, event, data) => {
     const [res] = await client.query("SELECT * FROM actions a, actions_connections ac, guilds g WHERE ac.guild = g.snowflake AND ac.action = a.id AND g.snowflake = ? AND CheckRuleset(?, a.eventRules) = true", [guild.id, event]);
 
     for(var action of res) {
-        console.log("Found action " + action.name + " to be run.");
+        var code = sl(`
+            import {
+                Client,
+                Channel,
+                EventType,
+                Event,
+                NewMessageEvent,
+                Message,
+                SentMessage,
+                Guild,
+                EmbedField,
+                EmbedFooter,
+                EmbedAuthor,
+                Embed,
+                User
+            } from "./actions/lib/main.ts";
+
+            const client = await Client.newClient("692837502117216307", "654725534365909043", ${JSON.stringify(data)}, ${action.env});
+        `);
+        code += action.code;
+
+        try {
+            var script = null;
+
+            var timeout = setTimeout(() => {
+                console.log("Deno timeout");
+                script.kill();
+            }, 2e4);
+
+            /* eslint-disable no-inner-declarations */
+            function end(err, stdout, stderr) {
+                if(err) {
+                    console.error(err);
+                }
+
+                clearTimeout(timeout);
+                console.log({ stdout });
+                console.warn({ stderr });
+            }
+            /* eslint-enable no-inner-declarations */
+
+            try {
+                code = `'${code.replace(/'/g, "'\\''")}'`;
+                script = exec("PWD=" + join(__dirname, "../../actions/") + "NO_COLOR=true ~/.local/bin/deno eval " + code, end);
+            } catch(e) {
+                console.error("[error_cmd]", e);
+                return;
+            }
+        } catch(e) {
+            console.error("Action failed");
+        }
     }
 
     console.log("Actions done");
