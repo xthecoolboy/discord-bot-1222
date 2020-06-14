2e7af566d4476fa5a5afc7f039cb943acafb27a8
Daniel Bulant <admin@danbulant.eu>
2020-06-14 15:11:29 +0200

    Promisify

--- managers/accountManager.js
+++ managers/accountManager.js
@@ -2,49 +2,40 @@ const pool = require("./pool_mysql");
 const { padWithZeroes, insertAt } = require("../utils");
 const newEmbed = require("../embed");
 
-function getUser(id, uuid = false) {
-    return new Promise(function(resolve, reject) {
-        var query;
-        if(uuid) {
-            query = `SELECT  * FROM users WHERE uuid="${id}"`;
-        } else {
-            query = `SELECT  * FROM users WHERE discord="${id}"`;
-        }
-        pool.query(query, function(error, results, fields) {
-            if(error) return reject(error);
+async function getUser(id, uuid = false) {
+    var query;
+    if(uuid) {
+        query = "SELECT * FROM users WHERE uuid=?";
+    } else {
+        query = "SELECT * FROM users WHERE discord=?";
+    }
+    var [results] = await pool.query(query, [id]);
 
-            if(results === undefined) return resolve(null);
+    if(results === undefined) return null;
 
-            resolve(results[0]);
-        });
-    });
+    return results[0];
 }
-function fetchUserId(id) {
-    return new Promise(function(resolve, reject) {
-        var query = `SELECT  * FROM users WHERE id="${id}"`;
-        pool.query(query, function(error, results, fields) {
-            if(error) return reject(error);
-            if(!results) return resolve(null);
-            resolve(results[0]);
-        });
-    });
+async function fetchUserId(id) {
+    var query = `SELECT  * FROM users WHERE id="${id}"`;
+    var [results] = await pool.query(query);
+    if(!results) return null;
+    return results[0];
 }
 
-function createUser(id) {
-    return new Promise(function(resolve, reject) {
-        var uuid = "unhex(replace(uuid(),'-',''))";
-        var query = `INSERT INTO users (uuid_bin, discord) VALUE (${uuid}, ${id})`;
-        pool.query(query, function(error, results, fields) {
-            if(error) return reject(error);
-            var query2 = `SELECT  * FROM users WHERE id="${results.insertId}"`;
-            pool.query(query2, (error, results, fields) => {
-                if(error) return reject(error);
-                resolve(results[0]);
-            });
-        });
-    });
+async function createUser(id) {
+    var uuid = "unhex(replace(uuid(),'-',''))";
+    var query = "INSERT INTO users (uuid_bin, discord) VALUE (?, ?)";
+    var [results] = await pool.query(query, [uuid, id]);
+    var query2 = "SELECT  * FROM users WHERE id=?";
+    var [user] = await pool.query(query2, [results.insertId]);
+    return user[0];
 }
 
+/**
+ * Fetches user from DB.
+ * @param {number|string} id to fetch
+ * @param {boolean} uuid if in UUID mode
+ */
 async function fetchUser(id, uuid = false) {
     var user = await getUser(id, uuid);
     if(!user && !uuid) {
@@ -122,77 +113,65 @@ function getPrevLevel(xp) {
 }
 
 function getMoney(user) {
-    var bbs = padWithZeroes(user.bbs, 4);
+    var bbs = user.bbs.toString().padStart(4, "0");;
     return insertAt(bbs, ".", bbs.length - 3) + " BBS";
 }
 
-function mine(user) {
-    return new Promise((resolve, reject) => {
-        var d;
-        try {
-            var t = user.last_mined.split(/[- :]/);
-            d = new Date(Date.UTC(t[0], t[1] - 1, t[2], t[3], t[4], t[5]));
-        } catch(e) {
-            d = user.last_mined;
-        }
+async function mine(user) {
+    var d;
+    try {
+        var t = user.last_mined.split(/[- :]/);
+        d = new Date(Date.UTC(t[0], t[1] - 1, t[2], t[3], t[4], t[5]));
+    } catch(e) {
+        d = user.last_mined;
+    }
 
-        var now = Date.now();
-        const oneDay = 60 * 60 * 12 * 1000;
+    var now = Date.now();
+    const oneDay = 60 * 60 * 12 * 1000;
 
-        try {
-            var canMine = (now - d) > oneDay;
-        } catch(e) {
-            canMine = true;
-        }
+    try {
+        var canMine = (now - d) > oneDay;
+    } catch(e) {
+        canMine = true;
+    }
 
-        if(canMine) {
-            var currentTimestamp = new Date().toISOString().slice(0, 19).replace("T", " ");
-            pool.query(`UPDATE users SET last_mined="${currentTimestamp}", bbs="${parseInt(user.bbs) + getLevel(user)}", xp="${parseInt(user.xp) + getLevel(user)}" WHERE uuid="${user.uuid}"`, (err) => {
-                if(err) return reject(err);
-                resolve(parseInt(user.bbs) + getLevel(user));
-            });
-        } else {
-            resolve(oneDay - (now - d));
-        }
-    });
+    if(canMine) {
+        var currentTimestamp = new Date().toISOString().slice(0, 19).replace("T", " ");
+        await pool.query(
+            "UPDATE users SET last_mined=?, bbs=?, xp=? WHERE uuid=?",
+            [currentTimestamp, (parseInt(user.bbs) + getLevel(user)), parseInt(user.xp) + getLevel(user), user.uuid]
+        );
+        return true;
+    } else {
+        return (oneDay - (now - d));
+    }
 }
 
-function achievments(id) {
-    return new Promise(function(resolve, reject) {
-        var query = `SELECT *, u.xp as u_xp FROM users u, achievments_awarded d, achievments a WHERE u.id="${id}" AND d.achievment = a.id AND d.user = u.id ORDER BY d.id DESC`;
-
-        pool.query(query, function(error, results, fields) {
-            if(error) reject(error);
-            resolve(results);
-        });
-    });
+async function pay(src, target, bbs) {
+    await pool.query("UPDATE users SET BBS=BBS + ? WHERE id=?", [bbs, target]);
+    await pool.query("UPDATE users SET BBS=BBS - ? WHERE id=?", [bbs, src]);
 }
 
-function awardAchievment(id, code, msg) {
-    return new Promise(function(resolve, reject) {
-        var query = `INSERT INTO achievments_awarded (user, achievment) VALUES (${id}, (SELECT id FROM achievments WHERE code='${code}'))`;
+async function achievments(id) {
+    var query = "SELECT *, u.xp as u_xp FROM users u, achievments_awarded d, achievments a WHERE u.id=? AND d.achievment = a.id AND d.user = u.id ORDER BY d.id DESC";
 
-        pool.query(query, function(error, results, fields) {
-            if(error) reject(error);
-            pool.query("SELECT * FROM achievments WHERE code='" + code + "'", (er, ac, fi) => {
-                if(er) reject(er);
-                var a = ac[0];
-                pool.query(`UPDATE users SET bbs = bbs + ${a.value}, xp = xp + ${a.xp} WHERE id = ${id}`, async (e, res, f) => {
-                    if(e) reject(e);
-                    var level = updateLevels(await fetchUserId(id));
-                    if(level) {
-                        pool.query(`SELECT * FROM users WHERE id=${id}`, (erro, res) => {
-                            if(erro) reject(erro);
-                            levelUp(level, msg, res[0]);
-                            resolve(results);
-                        });
-                    } else {
-                        resolve(results);
-                    }
-                });
-            });
-        });
-    });
+    var [res] = await pool.query(query, [id]);
+    return res;
+}
+
+async function awardAchievment(id, code, msg) {
+    var query = "INSERT INTO achievments_awarded (user, achievment) VALUES (?, (SELECT id FROM achievments WHERE code=?))";
+
+    var [results] = await pool.query(query, [id, code]);
+    var [ac] = pool.query("SELECT * FROM achievments WHERE code=?", [code]);
+    var a = ac[0];
+    await pool.query("UPDATE users SET bbs = bbs + ?, xp = xp + ? WHERE id = ?", [a.value, a.xp, id]);
+    var level = updateLevels(await fetchUserId(id));
+    if(level) {
+        var [res] = await pool.query("SELECT * FROM users WHERE id=?", [id]);
+        levelUp(level, msg, res[0]);
+    }
+    return results;
 }
 
 async function levelUp(level, msg, user) {
@@ -243,6 +222,7 @@ function updateLevels(user) {
 
 module.exports = {
     getUser,
+    pay,
     createUser,
     fetchUser,
     fetchUserUUID,
