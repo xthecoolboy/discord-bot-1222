0511124ec32210387eb9abbb2d57ed3b068a54ba
Daniel Bulant <admin@danbulant.eu>
2020-06-14 14:00:05 +0200

    Show how much time is needed until next mine

--- cmd/balance/mine.js
+++ cmd/balance/mine.js
@@ -1,8 +1,6 @@
 const commando = require("@iceprod/discord.js-commando");
 const account = require("../../managers/accountManager");
-const TimeAgo = require("javascript-time-ago");
-const en = require("javascript-time-ago/locale/en");
-TimeAgo.addLocale(en);
+const moment = require("moment");
 
 module.exports = class Mine extends commando.Command {
     constructor(client) {
@@ -17,10 +15,10 @@ module.exports = class Mine extends commando.Command {
     async run(msg) {
         try {
             var mined = await account.mine(await account.fetchUser(msg.author.id));
-            if(mined) {
+            if(mined === true) {
                 msg.channel.send("Successfully mined BBS! Your current balance is " + await account.getMoney(await account.fetchUser(msg.author.id)));
             } else {
-                msg.channel.send("You can't mine yet.");
+                msg.channel.send("You can't mine yet. Try again " + (moment(new Date(mined + Date.now())).fromNow() || "later") + ".");
             }
         } catch(e) {
             console.warn(e);
