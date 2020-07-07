036bf034c55729e60926716a3af63f6ca3f7b53f
Daniel Bulant <admin@danbulant.eu>
2020-07-07 11:53:24 +0200

    Use new OOP for account management

--- cmd/balance/mine.js
+++ cmd/balance/mine.js
@@ -1,5 +1,4 @@
 const commando = require("@iceprod/discord.js-commando");
-const account = require("../../managers/accountManager");
 const moment = require("moment");
 
 module.exports = class Mine extends commando.Command {
@@ -14,14 +13,16 @@ module.exports = class Mine extends commando.Command {
 
     async run(msg) {
         var lang = await msg.guild.lang();
-        var localLocale = moment();
-        localLocale.locale(await msg.guild.settings.get("lang", "en"));
+        await msg.author.fetchUser();
         try {
-            var mined = await account.mine(await account.fetchUser(msg.author.id));
+            var mined = await msg.author.mine();
             if(mined === true) {
-                msg.channel.send(lang.mine.done.replace("%s", await account.getMoney(await account.fetchUser(msg.author.id))));
+                msg.channel.send(lang.mine.done.replace("%s", await msg.author.money));
             } else {
-                msg.channel.send(lang.mine.not_yet + (localLocale(new Date(mined + Date.now())).fromNow()) + ".");
+                const localLocale = moment(new Date(mined + Date.now()));
+                localLocale.locale(await msg.guild.settings.get("lang", "en"));
+                console.log(localLocale);
+                msg.channel.send(lang.mine.not_yet + (localLocale.fromNow() || "later") + ".");
             }
         } catch(e) {
             console.warn(e);
