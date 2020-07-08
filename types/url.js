5bdf2cb9f97df777cb14739177aa20d583ac632a
Daniel Bulant <admin@danbulant.eu>
2020-07-08 16:18:42 +0200

    Fix the URL type

--- types/url.js
+++ types/url.js
@@ -11,12 +11,18 @@ class URLArgumentType extends ArgumentType {
     }
 
     validate(val, msg, arg) {
-        var pattern = new RegExp("^(https?:\\/\\/)?" + // protocol
+        var pattern = new RegExp("^https?:\\/\\/" + // protocol
         "((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|" + // domain name
         "((\\d{1,3}\\.){3}\\d{1,3}))" + // OR ip (v4) address
         "(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*" + // port and path
         "(\\?[;&a-z\\d%_.~+=-]*)?" + // query string
         "(\\#[-a-z\\d_]*)?$", "i"); // fragment locator
+        try {
+            var s = new URL(val);
+            s.toString();
+        } catch(e) {
+            return false;
+        }
         return !!pattern.test(val);
     }
 }
