a9b0cd51e1783405d50b7356e545dde900aa39a7
Daniel Bulant <admin@danbulant.eu>
2020-07-08 13:32:07 +0200

    Fix the oldest command

--- cmd/essentials/oldest.js
+++ cmd/essentials/oldest.js
@@ -16,7 +16,13 @@ module.exports = class Oldest extends commando.Command {
     async run(msg) {
         var lang = await msg.guild.lang();
         var list = msg.guild.members.cache;
-        list = list.sort((userA, userB) => userA.createdTimestamp - userB.createdTimestamp);
+
+        list = list.sort((userA, userB) => {
+            var res = userA.user.createdTimestamp - userB.user.createdTimestamp;
+            if(res === 0) return 0;
+            if(res < 0) return -1;
+            if(res > 0) return 1;
+        });
         list = list.array();
 
         var embed = newEmbed();
@@ -27,6 +33,14 @@ module.exports = class Oldest extends commando.Command {
 
         var em = await msg.channel.send(embed);
 
-        await pages(msg, embed, em, list, 1, lang.oldest.pages, user => `${user.user.tag} (${user.displayName} @ ${user.user.createdAt.getUTCDate()}.${user.user.createdAt.getUTCMonth() + 1}. ${user.user.createdAt.getUTCFullYear()})`);
+        await pages(
+            msg,
+            embed,
+            em,
+            list,
+            1,
+            lang.oldest.pages,
+            user => `${user.user.tag} (${user.displayName} @ ${user.user.createdAt.getUTCDate()}.${user.user.createdAt.getUTCMonth() + 1}. ${user.user.createdAt.getUTCFullYear()})`
+        );
     }
 };
