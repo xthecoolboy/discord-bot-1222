a40afeebd70bf033c360783fc1e20d8824e986e8
Daniel Bulant <admin@danbulant.eu>
2020-07-08 13:35:03 +0200

    Format stats command

--- cmd/mod/stats.js
+++ cmd/mod/stats.js
@@ -6,7 +6,6 @@ const timeAgo = new TimeAgo("en-US");
 const { shortNumber } = require("../../utils");
 
 global.started = new Date();
-global.lastReload = new Date();
 
 module.exports = class Stats extends commando.Command {
     constructor(client) {
@@ -25,7 +24,7 @@ module.exports = class Stats extends commando.Command {
             var embed = newEmbed();
             embed.setTitle("Aztec");
             embed.setDescription("The most universal bot.");
-            embed.setURL("https://iceproductions.dev");
+            embed.setURL("https://aztec.bot");
 
             embed.setThumbnail(this.client.user.avatarURL());
             var users = 0;
@@ -35,13 +34,12 @@ module.exports = class Stats extends commando.Command {
                 users += guild[1].memberCount;
             }
 
-            embed.addField("Website", "[iceproductions.dev](https://iceproductions.dev)", true);
+            embed.addField("Website", "[aztec.bot](https://aztec.bot)", true);
             embed.addField("Main guild", "[Aztec support](https://discord.gg/8fqEepV)", true);
             embed.addField("Prefix", "`" + msg.guild.commandPrefix + "`", true);
             embed.addField("Users", shortNumber(users), true);
             embed.addField("Guilds", shortNumber(guilds), true);
-            embed.addField("Uptime", timeAgo.format(global.started), true);
-            // embed.addField("Last reloaded", timeAgo.format(global.lastReload), true);
+            embed.addField("Last restart", timeAgo.format(global.started), true);
             msg.channel.send(embed);
         } catch(e) {
             msg.channel.send("An error occured.");
