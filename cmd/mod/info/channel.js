90d5f1f9ff586f1461b5ebb86aa2873e0618996a
noahmanscher <noahmanscher@gmail.com>
2020-07-09 18:36:12 +0200

    create channel.js

--- cmd/mod/info/channel.js
+++ cmd/mod/info/channel.js
@@ -1,6 +1,13 @@
-
 const newEmbed = require("../../../embed");
 
 module.exports = (msg, cmd) => {
-    msg.channel.send(newEmbed().setTitle("To be done"));
+    const embed = newEmbed()
+        .setAuthor(msg.author.tag, msg.author.avatarURL())
+        .setTitle(msg.channel.name)
+        .setDescription(msg.channel.topic)
+        .addField("ID", msg.channel.id, true)
+        .addField("NSFW", msg.channel.nsfw, true)
+        .addField("Members", msg.channel.members.size, true);
+    if(msg.guild.bannerURL) embed.setImage(msg.guild.bannerURL);
+    return msg.embed(embed);
 };
