3e8aa9f155e158dd8b60370862ff70390671d069
noahmanscher <noahmanscher@gmail.com>
2020-07-09 19:37:09 +0200

    allow selecting of channel

--- cmd/mod/info/channel.js
+++ cmd/mod/info/channel.js
@@ -1,13 +1,15 @@
 const newEmbed = require("../../../embed");
 
 module.exports = (msg, cmd) => {
+    const channel = (cmd.pointer ? msg.guild.channels.resolve(cmd.pointer) : msg.channel);
+    if(cmd.pointer && !channel) return msg.say("Channel not found");
+
     const embed = newEmbed()
         .setAuthor(msg.author.tag, msg.author.avatarURL())
-        .setTitle(msg.channel.name)
-        .setDescription(msg.channel.topic)
-        .addField("ID", msg.channel.id, true)
-        .addField("NSFW", msg.channel.nsfw, true)
-        .addField("Members", msg.channel.members.size, true);
-    if(msg.guild.bannerURL) embed.setImage(msg.guild.bannerURL);
+        .setTitle(channel.name)
+        .addField("ID", channel.id, true)
+        .addField("NSFW", channel.nsfw ? "Yes :smiling_imp:" : "No :slight_smile:", true)
+        .addField("Members", channel.members.size, true);
+    if(channel.topic) embed.setDescription(channel.topic);
     return msg.embed(embed);
 };
